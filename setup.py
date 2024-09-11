8ac2d36712e7f7ac734f78c7ec8bbbb44e0ddf0f
dan ballard <arvellon@MacBook-Air.lan>
2024-09-10 21:51:03 -0700

    Update workflow and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='defi-file-storage',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+        # e.g., 'flask==2.2.2',
+    ],
+)
